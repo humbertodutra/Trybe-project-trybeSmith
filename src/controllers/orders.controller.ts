07d8ede02db57145d8dd1d773476254f3f6ced19
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-09-21 11:41:59 -0300

    100% - exercise 8

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { list, create } from '../services/order.services';
 import Token from '../interfaces/token';
+import verify from '../errorMiddleware';
 
 const orderList = async (req: Request, res: Response) => {
   const result = await list();
@@ -11,11 +12,15 @@ const createController = async (req: Request, res: Response) => {
   const token = {
     token: req.headers.authorization,
   };
-
   const { productsIds } = req.body;
+  console.log(productsIds);
+  
+  const verifyReq = verify(productsIds);
+  if (verifyReq !== null) {
+    return res.status(verifyReq.status).json(verifyReq.message);
+  }
   const result = await create(token as Token, productsIds);
-
-  return res.status(200).json(result);
+  return res.status(201).json(result);
 };
 
 export {
---
+++ src/errorMiddleware.ts
@@ -0,0 +1,17 @@
+const verify = (productsIds: number[]) => {
+  const products = productsIds;
+  if (products === undefined) {
+    return { status: 400, message: { message: '"productsIds" is required' } };
+  }
+
+  if (products.constructor !== Array) {
+    return { status: 422, message: { message: '"productsIds" must be an array' } };
+  }
+
+  if (products.length === 0) {
+    return { status: 422, message: { message: '"productsIds" must include only numbers' } };
+  }
+  return null;
+};
+
+export default verify;
